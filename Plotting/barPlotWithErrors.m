ef51c62cb140ff0dd467aa1f8ed99018e969207c
alex <alexgb2010@gmail.com>
2015-01-16 17:20:32 -0800

    added plotting code for channel counts and for individiaul subjects

--- Plotting/barPlotWithErrors.m
+++ Plotting/barPlotWithErrors.m
@@ -10,9 +10,9 @@ function barPlotWithErrors(ha,M,opts)
 %   yLimits 
 %   xPositions
 
-axes(ha); hold on;
+axes(ha); cla; hold on;
 
-nBars   = size(M,1);
+nBars   = numel(M);
 m       = zeros(nBars,1);
 se      = zeros(nBars,1);
 
@@ -32,6 +32,15 @@ if ~isfield(opts,'xPositions')
     opts.xPositions = 1:nBars;
 end
 
+if isfield(opts,'xLimits')
+    xlim(opts.xLimits)
+else
+    xlim([0 nBars]+0.5)
+end
+
+plot(xlim,[opts.baseLine opts.baseLine],'-',...
+    'color',ones(3,1)*0.3,'linewidth',2)
+
 for ba = 1:nBars
     
     X = M{ba};
@@ -48,15 +57,6 @@ if isfield(opts,'yLimits')
     ylim(opts.yLimits)
 end
 
-if isfield(opts,'xLimits')
-    xlim(opts.xLimits)
-else
-    xlim([0 nBars]+0.5)
-end
-
-plot(xlim,[opts.baseLine opts.baseLine],'-',...
-    'color',[0 0 0],'linewidth',2)
-
 if isfield(opts,'xTicks')
    set(gca,'XTick',opts.xTicks)
 else
@@ -68,7 +68,6 @@ if isfield(opts,'yTicks')
 else
     set(gca,'yTick',[])
 end
-
 
 set(gca,'LineWidth',2,'FontSize',18,'XTickLabel','')
 
